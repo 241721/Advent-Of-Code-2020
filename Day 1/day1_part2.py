c692fc3eda8965dd22890beeecb4c9fe23e311f9
241721 <39262993+241721@users.noreply.github.com>
2020-12-03 19:04:57 +0100

    Update day1_part2.py

--- Day 1/day1_part2.py
+++ Day 1/day1_part2.py
@@ -1,16 +1,17 @@
+from itertools import combinations
+
 def read_input(filename):
     with open(filename) as f:
         return set(map(int, f))
 
 def day1(input):
-    for i in L:
-        for j in L:
-            for l in L:
-                if i+j+l == 2020:
-                    return i*j*l
-
+    L = list(combinations(input, 3))
+    for l in L:
+        if sum(l) == 2020:
+            return l[0] * l[1] * l[2]
 
 if __name__ == '__main__':
     L = read_input("input.py")
     print(day1(L))
+
 
