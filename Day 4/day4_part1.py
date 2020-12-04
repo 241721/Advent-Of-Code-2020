9c536d39b49e12a8a4fdc0b87430308f6c5e45d1
241721 <39262993+241721@users.noreply.github.com>
2020-12-04 22:40:51 +0100

    Create day4_part1.py

---
+++ Day 4/day4_part1.py
@@ -0,0 +1,44 @@
+def read_input(filename):
+    file = open('input.py', 'r')
+    Lines = file.readlines()
+    Lines = [x.strip() for x in Lines]
+    return Lines
+
+
+def is_passport_valid(passport):
+    obligatory_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    for key in obligatory_keys:
+        if key not in passport:
+            print("{} {}".format(passport, key))
+            return False
+    return True
+
+
+def pasre_input_to_passports(input):
+    passports = []
+    passport = {}
+
+    for line in input:
+        if not (len(line)) == 0:
+            elements = line.split(" ")
+            for element in elements:
+                element = element.split(":")
+                passport.update({element[0]: element[1]})
+        else:
+            passports.append(dict(passport))
+            passport.clear()
+    return passports
+
+
+if __name__ == '__main__':
+    input = read_input("input.py")
+    passports = pasre_input_to_passports(input)
+
+    valid_passports = 0
+    for passport in passports:
+        if is_passport_valid(passport):
+            valid_passports += 1
+
+    print(valid_passports+1)
+
+
